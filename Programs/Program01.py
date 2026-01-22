45e66b76fe118edb95b20c219948a001e4fe98b3
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-01-22 11:58:32 +0530

    First Code

---
+++ Programs/Program01.py
@@ -0,0 +1,7 @@
+
+# printf("Jay Ganesh...");                  in C Programming
+# cout << "Jay Ganesh...";                  in C++ Programming
+# System.out.println("Jay Ganesh...");      in Java Programming
+# console.log("Jay Ganesh...")              in javascript Programming
+
+print("Jay Ganesh..!")
