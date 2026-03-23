87b24dc94d475c7c689f6958d7589ee040ddaaaa
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-03-23 16:41:57 +0530

    create header file like c

---
+++ Programs/Program54.py
@@ -0,0 +1,10 @@
+# include<Sample.h>         in C
+# using namespace Sample    in C++
+# import Sample.*           in Java
+
+import Sample
+
+print("Inside main application")
+print("__name__ from main application is : ", __name__)
+
+Sample.Display()  
