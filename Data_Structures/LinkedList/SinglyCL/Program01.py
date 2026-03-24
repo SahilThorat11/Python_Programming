18338a7abda72c31cf43526b9444fbba025d9d05
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-03-24 19:55:49 +0530

    Function for insert data into first position

---
+++ Data_Structures/LinkedList/SinglyCL/Program01.py
@@ -0,0 +1,70 @@
+# DONE
+class Node:
+    def __init__(self, value):
+        self.data = value
+        self.next = None
+
+class SinglyCL:
+
+    # DONE
+    def __init__(self):
+        self.first = None
+        self.iCount = 0
+
+    def InsertFirst(self, no):
+        newn = Node(no) 
+
+        # LL is empty
+        if (self.first == None):
+            self.first = newn
+            newn.next = self.first
+        #LL contains atleast one node
+        else:
+            temp = self.first
+
+            while(temp.next != self.first):
+                temp = temp.next
+
+            newn.next = self.first
+            self.first = newn
+            temp.next = self.first
+
+        self.iCount = self.iCount + 1
+            
+    
+    def InsertLast(self, no):
+        pass
+
+    def InsertAtPos(self, no, pos):
+        pass
+
+    def DeleteFirst(self):
+        pass
+
+    def DeleteLast(self):
+        pass
+    
+    def DeleteAtPos(self, pos):
+        pass
+    
+    def Display(self):
+        pass
+
+    # DONE 
+    def Count(self):
+        return self.iCount
+
+def main():
+
+    sobj = SinglyCL()
+    
+    sobj.InsertFirst(101)
+    sobj.InsertFirst(51)
+    sobj.InsertFirst(21)
+    sobj.InsertFirst(11)
+
+    
+if __name__ == "__main__":
+    main()
+
+    
