5b694ae2266929845b3a65124a0a8ecee053aee0
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-03-28 19:39:03 +0530

    Function to insert data at first position

---
+++ Data_Structures/LinkedList/DoublyCL/Program02.py
@@ -0,0 +1,73 @@
+# DONE
+class Node:
+    def __init__(self, value):
+        self.data = value
+        self.next = None
+        self.prev = None
+
+class DoublyCL:
+
+    # DONE
+    def __init__(self):
+        self.first = None
+        self.last = None
+        self.iCount = 0
+
+    # DONE
+    def InsertFirst(self, no):
+        newn = Node(no)
+
+        # if LL is empty
+        if((self.first == None) and (self.last == None)):
+            self.first = newn
+            self.last = newn
+        # If LL is conatins atleast one node
+        else:
+            newn.next = self.first
+            self.first.prev = newn
+            self.first = newn
+
+        self.first.prev = self.last
+        self.last.next = self.first
+
+        self.iCount = self.iCount + 1
+    
+    def InsertLast(self, no):
+        pass
+
+    def InsertAtPos(self, no, pos):
+        pass
+
+    def DeleteFirst(self):
+        pass
+
+    def DeleteLast(self):
+        pass
+    
+    def DeleteAtPos(self, pos):
+        pass
+    
+    def Display(self):
+        pass
+
+    # DONE 
+    def Count(self):
+        return self.iCount
+
+def main():
+
+    dobj = DoublyCL()
+
+    dobj.InsertFirst(51)
+    dobj.InsertFirst(21)
+    dobj.InsertFirst(11)
+
+    print("Elements of Linked list are : ")
+    dobj.Display()
+    print("Number of elements in Linked list are : ", dobj.Count())
+    print()
+    
+if __name__ == "__main__":
+    main()
+
+    
