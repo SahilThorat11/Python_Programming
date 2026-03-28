6cc4c3c6f13371d1f7acb67f1cf150a83126c754
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-03-28 19:38:11 +0530

    Function for counting nodes

---
+++ Data_Structures/LinkedList/DoublyCL/Program01.py
@@ -0,0 +1,50 @@
+# DONE
+class Node:
+    def __init__(self, value):
+        self.data = value
+        self.next = None
+        self.prev = None
+
+class DoublyCL:
+
+    # DONE
+    def __init__(self):
+        self.first = None
+        self.last = None
+        self.iCount = 0
+
+    def InsertFirst(self, no):
+        pass
+    
+    def InsertLast(self, no):
+        pass
+
+    def InsertAtPos(self, no, pos):
+        pass
+
+    def DeleteFirst(self):
+        pass
+
+    def DeleteLast(self):
+        pass
+    
+    def DeleteAtPos(self, pos):
+        pass
+    
+    def Display(self):
+        pass
+
+    # DONE 
+    def Count(self):
+        return self.iCount
+
+def main():
+
+    dobj = DoublyCL()
+
+    print("Number of elements in Linked list are : ", dobj.Count())
+    
+if __name__ == "__main__":
+    main()
+
+    
