089c878b38800e65af896a31c1e8297eb7648014
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-03-29 15:30:37 +0530

    Class Creaion and function declaration

---
+++ Data_Structures/Stack/Program01.py
@@ -0,0 +1,31 @@
+
+class Node:
+    def __init__(self, no):
+        self.data = no
+        self.next = None
+
+
+class Stack:
+    def __init__(self):
+        pass
+
+    def Push(self, no):
+        pass
+
+    def Pop(self):
+        pass
+
+    def peep(self):
+        pass
+
+    def Display(self):
+        pass
+
+    def Count(self):
+        pass        
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
