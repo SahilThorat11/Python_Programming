69a3c7a2532271c508653b6cb3092d754bd109aa
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-03-29 15:34:08 +0530

    Constructor of Stack class

---
+++ Data_Structures/Stack/Program02.py
@@ -0,0 +1,34 @@
+
+class Node:
+    def __init__(self, no):
+        self.data = no
+        self.next = None
+
+
+class Stack:
+    
+    def __init__(self):
+        print("Stack gets created successfully...")
+        self.first = None
+        self.iCount = 0
+
+    def Push(self, no):
+        pass
+
+    def Pop(self):
+        pass
+
+    def peep(self):
+        pass
+
+    def Display(self):
+        pass
+
+    def Count(self):
+        pass        
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
