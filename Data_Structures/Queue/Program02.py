4761a4e4b87e56c75031326ff6a5b704e63457fb
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-04-02 11:46:24 +0530

    Constructor of Queue class initialization

---
+++ Data_Structures/Queue/Program02.py
@@ -0,0 +1,30 @@
+class QueueNode:
+    def __init__(self, no):
+        self.data = no
+        self.next = None
+
+
+class Queue:
+    def __init__(self):
+        print("Queue gets created successfully...\n")
+        self.first = None
+        self.iCount = 0
+
+    def enqueue(self, no):
+        pass
+
+    def dequeue(self):
+        pass
+
+    def display(self):
+        pass
+
+    def count(self):
+        pass
+
+
+def main():
+    qobj = Queue()
+
+if __name__ == "__main__":
+    main()
