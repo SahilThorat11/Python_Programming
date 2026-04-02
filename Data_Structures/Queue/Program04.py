0af5de46667ddf1788b49f957c0c010241cb53d8
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-04-02 11:52:49 +0530

    Display function for display the data from Queue

---
+++ Data_Structures/Queue/Program04.py
@@ -0,0 +1,55 @@
+class QueueNode:
+    def __init__(self, no):
+        self.data = no
+        self.next = None
+
+
+class Queue:
+    def __init__(self):
+        print("Queue gets created successfully...\n")
+        self.first = None
+        self.iCount = 0
+
+    def enqueue(self, no):
+        newn = QueueNode(no)
+
+        if(self.first == None):
+            self.first = newn
+        else:
+            temp = self.first
+            while(temp.next != None):
+                temp = temp.next
+            temp.next = newn
+
+        self.iCount = self.iCount + 1
+
+    def dequeue(self):
+        pass
+
+    def display(self):
+        if(self.first == None):
+            print("Queue is empty")
+            return
+
+        temp = self.first
+
+        while(temp.next != None):
+            print("|", temp.data, "|-", end="")
+            temp = temp.next
+
+    def count(self):
+        pass
+
+
+def main():
+    qobj = Queue()
+    
+    qobj.enqueue(11)
+    qobj.enqueue(21)
+    qobj.enqueue(51)
+    qobj.enqueue(101)
+
+    qobj.display()
+
+if __name__ == "__main__":
+    main()
