5f37126adad59596d95597137bacc8d1a60ac51f
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-04-02 11:44:54 +0530

    Node creation and function declaration

---
+++ Data_Structures/Queue/Program01.py
@@ -0,0 +1,28 @@
+class QueueNode:
+    def __init__(self, no):
+        self.data = no
+        self.next = None
+
+
+class Queue:
+    def __init__(self):
+        pass
+
+    def enqueue(self, no):
+        pass
+
+    def dequeue(self):
+        pass
+
+    def display(self):
+        pass
+
+    def count(self):
+        pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
