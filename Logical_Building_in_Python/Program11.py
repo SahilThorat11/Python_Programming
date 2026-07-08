564769498a9c86a33ee0f25d4bd6d8b424677b78
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-07-08 10:43:02 +0530

    Program to display Even factors

---
+++ Logical_Building_in_Python/Program11.py
@@ -0,0 +1,19 @@
+def DisplayEvenFactors(No):
+
+    if(No < 0):
+        No = -No
+        
+    for i in range(1,int((No/2)+1)):
+        if((No % i == 0) and (i % 2 == 0)):
+            print(i) 
+
+def main():
+    Value = 0
+
+    print("Enter number :")
+    Value = int(input())
+
+    DisplayEvenFactors(Value)       
+
+if __name__ == "__main__":
+    main()        
