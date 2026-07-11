abcc473399d45fcc07f8dfd0fa9817aca2e057b5
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-07-11 17:35:40 +0530

    Display Numbers till Nth

---
+++ Logical_Building_in_Python/Program25.py
@@ -0,0 +1,22 @@
+# Input : 4
+# Output : * * * *
+
+def Display(No):
+
+    if (No < 0):
+        No = - No
+
+    for i in range(1,No+1):
+        print("*\t",end="")
+    print("\n")    
+   
+def main():
+    Value = 0
+
+    print("Enter Frequency : ")
+    Value = int(input())
+
+    Display(Value)
+    
+if __name__ == "__main__":
+    main()        
