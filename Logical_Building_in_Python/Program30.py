2206f9a804fa40d74729a53c50c46e8f9104f2bd
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-07-12 15:31:08 +0530

    Display Numbers till given number

---
+++ Logical_Building_in_Python/Program30.py
@@ -0,0 +1,23 @@
+# Input : 4
+# Output : 4 3 2 1 0 
+
+def Display(No):
+
+    if (No < 0):
+        No = - No
+
+    i = No
+    for i in range(No,-1,-1):
+        print(i,"\t",end="")
+    print("\n")    
+   
+def main():
+    Value = 0
+
+    print("Enter Frequency : ")
+    Value = int(input())
+
+    Display(Value)
+    
+if __name__ == "__main__":
+    main()        
