2e48e9e4b7eb3bdf1955cff0de17d2da8bcdec05
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-07-16 12:31:35 +0530

    Problem on matrix

---
+++ Logical_Building_in_Python/Program39.py
@@ -0,0 +1,20 @@
+def Display(iRow,iCol):
+    
+    for i in range(iRow):
+        for j in range(iCol):
+            print("*\t",end="")
+        print("\n")
+
+            
+def main():
+
+    print("Enter number of rows:")
+    Value1 = int(input())
+
+    print("Enter number of cloumns:")
+    Value2 = int(input())
+
+    Display(Value1,Value2)
+
+if __name__ == "__main__":
+    main()        
