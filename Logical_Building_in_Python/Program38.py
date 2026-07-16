b119801de906cb0481c5425f1e25430e8af5b35b
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-07-16 12:31:14 +0530

    Display

---
+++ Logical_Building_in_Python/Program38.py
@@ -0,0 +1,13 @@
+def Display():
+    
+    for i in range(3):
+        for j in range(3):
+            print("*\t",end="")
+        print("\n")
+
+            
+def main():
+    Display()
+
+if __name__ == "__main__":
+    main()        
