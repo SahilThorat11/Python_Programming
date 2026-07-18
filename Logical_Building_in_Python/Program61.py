292a61f78a0550df7e0603c9eff272d46c204d27
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-07-18 12:36:55 +0530

    Problem on matrix

---
+++ Logical_Building_in_Python/Program61.py
@@ -0,0 +1,37 @@
+# Input : 4 4
+
+#         *
+#       * *
+#     * * *        
+#   * * * *
+
+class Pattern:
+
+    def Display(self,iRow,iCol):
+
+        if iRow != iCol:
+            print("Invaild Input")
+            return
+
+        for i in range(1,iRow + 1):
+            for j in range(iCol,0,-1):
+                if i >= j:
+                    print("*\t",end="")
+                else:
+                    print(" \t",end="")
+            print("\n")   
+
+def main():
+
+    print("Enter number of rows:")
+    Value1 = int(input())
+
+    print("Enter number of cloumns:")
+    Value2 = int(input())
+
+    pobj = Pattern()
+
+    pobj.Display(Value1,Value2)
+
+if __name__ == "__main__":
+    main()        
